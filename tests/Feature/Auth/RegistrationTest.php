d49786f059cec6dfd18d0c4f35ed0725dfb620b2
Arnaud PONEL <arnaud@xpressive.io>
2025-02-07 16:39:44 +0100

    FIX : Login error after registration (#1)
    * Used event to attach an empty wallet to newly registered users
    
    * Added test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Models\User;
 use function Pest\Laravel\assertAuthenticated;
 use function Pest\Laravel\get;
 use function Pest\Laravel\post;
+use function PHPUnit\Framework\assertTrue;
 
 test('registration screen can be rendered', function () {
     $response = get('/register');
@@ -21,5 +23,10 @@ test('new users can register', function () {
     ]);
 
     assertAuthenticated();
+
     $response->assertRedirect(route('dashboard', absolute: false));
+
+    $user = User::whereEmail('test@example.com')->first();
+
+    assertTrue($user->wallet !== null);
 });
